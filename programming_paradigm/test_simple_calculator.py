e73f07729238d26950178ad0401f32007c0a4d73
mari498 <maria2006amir@gmail.com>
2025-06-21 20:14:05 +0300

    Final fix: test names for ALX checker

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -15,11 +15,11 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.subtract(10, 5), 5)
         self.assertEqual(self.calc.subtract(0, 10), -10)
 
-    def test_multiply(self):
+    def test_multipication(self):
         self.assertEqual(self.calc.multiply(4, 5), 20)
         self.assertEqual(self.calc.multiply(0, 10), 0)
 
-    def test_divide(self):
+    def test_division(self):
         self.assertEqual(self.calc.divide(10, 2), 5)
         self.assertIsNone(self.calc.divide(10, 0))
 
