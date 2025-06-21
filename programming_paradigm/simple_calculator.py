591026afac6d69a964fb9ada66a79b8b09bc0159
mari498 <maria2006amir@gmail.com>
2025-06-21 18:12:20 +0300

    Fix final unit test structure for ALX checker

--- programming_paradigm/simple_calculator.py
+++ programming_paradigm/simple_calculator.py
@@ -13,3 +13,4 @@ class SimpleCalculator:
             return None
         return a / b
 
+
--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -6,26 +6,20 @@ class TestSimpleCalculator(unittest.TestCase):
     def setUp(self):
         self.calc = SimpleCalculator()
 
-    def test_add(self):
+    def test_addition(self):
         self.assertEqual(self.calc.add(1, 1), 2)
         self.assertEqual(self.calc.add(-1, -5), -6)
         self.assertEqual(self.calc.add(0, 5), 5)
-        self.assertEqual(self.calc.add(1.5, 2.5), 4.0)
 
-    def test_subtract(self):
+    def test_subtraction(self):
         self.assertEqual(self.calc.subtract(10, 5), 5)
         self.assertEqual(self.calc.subtract(0, 10), -10)
-        self.assertEqual(self.calc.subtract(-3, -3), 0)
 
     def test_multiply(self):
         self.assertEqual(self.calc.multiply(4, 5), 20)
-        self.assertEqual(self.calc.multiply(-1, 5), -5)
-        self.assertEqual(self.calc.multiply(0, 100), 0)
-        self.assertEqual(self.calc.multiply(2.5, 2), 5.0)
+        self.assertEqual(self.calc.multiply(0, 10), 0)
 
     def test_divide(self):
         self.assertEqual(self.calc.divide(10, 2), 5)
-        self.assertEqual(self.calc.divide(5, 2), 2.5)
-        self.assertEqual(self.calc.divide(-6, 3), -2)
-        self.assertIsNone(self.calc.divide(10, 0))  # Division by zero
+        self.assertIsNone(self.calc.divide(10, 0))
 
