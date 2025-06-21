c4c92a8216d614dc19af68a85633dbfeb1978a8f
mari498 <maria2006amir@gmail.com>
2025-06-21 17:52:41 +0300

    Add SimpleCalculator class and its unit tests

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,31 @@
+import unittest
+from simple_calculator import SimpleCalculator
+
+class TestSimpleCalculator(unittest.TestCase):
+
+    def setUp(self):
+        self.calc = SimpleCalculator()
+
+    def test_add(self):
+        self.assertEqual(self.calc.add(1, 1), 2)
+        self.assertEqual(self.calc.add(-1, -5), -6)
+        self.assertEqual(self.calc.add(0, 5), 5)
+        self.assertEqual(self.calc.add(1.5, 2.5), 4.0)
+
+    def test_subtract(self):
+        self.assertEqual(self.calc.subtract(10, 5), 5)
+        self.assertEqual(self.calc.subtract(0, 10), -10)
+        self.assertEqual(self.calc.subtract(-3, -3), 0)
+
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(4, 5), 20)
+        self.assertEqual(self.calc.multiply(-1, 5), -5)
+        self.assertEqual(self.calc.multiply(0, 100), 0)
+        self.assertEqual(self.calc.multiply(2.5, 2), 5.0)
+
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(10, 2), 5)
+        self.assertEqual(self.calc.divide(5, 2), 2.5)
+        self.assertEqual(self.calc.divide(-6, 3), -2)
+        self.assertIsNone(self.calc.divide(10, 0))  # Division by zero
+
