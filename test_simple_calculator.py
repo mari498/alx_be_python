6b2fdb1707683fe9806e9397f210c0d52b457cd3
mari498 <maria2006amir@gmail.com>
2025-06-21 17:56:19 +0300

    Fix: Rename test methods to match ALX checker requirements

---
+++ test_simple_calculator.py
@@ -0,0 +1,16 @@
+def test_addition(self):
+    self.assertEqual(self.calc.add(1, 1), 2)
+    # باقي الاختبارات...
+
+def test_subtraction(self):
+    self.assertEqual(self.calc.subtract(10, 5), 5)
+    # باقي الاختبارات...
+
+def test_multiply(self):
+    self.assertEqual(self.calc.multiply(4, 5), 20)
+    # باقي الاختبارات...
+
+def test_divide(self):
+    self.assertEqual(self.calc.divide(10, 2), 5)
+    # باقي الاختبارات...
+
