47db2925d2570142b587b1950e503c62f819c801
mari498 <maria2006amir@gmail.com>
2025-06-21 18:19:04 +0300

    Fix import error and test names for ALX checker

--- test_simple_calculator.py
+++ test_simple_calculator.py
@@ -20,13 +20,13 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.subtract(0, 10), -10)
         self.assertEqual(self.calc.subtract(-3, -3), 0)
 
-    def test_multiply(self):
+    def test_multiplication(self):
         self.assertEqual(self.calc.multiply(4, 5), 20)
         self.assertEqual(self.calc.multiply(-1, 5), -5)
         self.assertEqual(self.calc.multiply(0, 100), 0)
         self.assertEqual(self.calc.multiply(2.5, 2), 5.0)
 
-    def test_divide(self):
+    def test_division(self):
         self.assertEqual(self.calc.divide(10, 2), 5)
         self.assertEqual(self.calc.divide(5, 2), 2.5)
         self.assertEqual(self.calc.divide(-6, 3), -2)
